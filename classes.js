66286a97fe579c8b603aa5b95c53dd7111537fc6
Shelley Thompson <shelleythompson@Shelleys-iMac.local>
2017-01-25 22:50:19 +1300

    created function three - purple

--- classes.js
+++ classes.js
@@ -7,6 +7,7 @@ function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
 one()
 two()
+three()
 
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -28,4 +29,8 @@ function two () {
 
 // CREATE FUNCTION three HERE
 
+function three() {
+  var three = document.getElementById('three')
+  three.classList.add('purple')
+}
 // CREATE FUNCTION makeVisible HERE
